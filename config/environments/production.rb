ad93996395f7d333fb5dc3787704990bfcee9512
egaunder <egaunder@gmail.com>
2016-07-23 08:29:24 -0700

    setup email functionality for sign_up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,12 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # SENDGRID setup option
+  # for development, don't send any emails'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://elvan-photo-app.herokuapp.com', 
+                                               :protocol => 'https'}
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
