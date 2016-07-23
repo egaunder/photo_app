4cbf7d6c6745590c0971fb8e324e1d2baf80ee1f
egaunder <egaunder@gmail.com>
2016-07-23 05:19:14 -0700

    add devise and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # add before_action to activate devise
   before_action :authenticate_user!
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
-  # skip the before action for only index 
+  # skip the before action for only index, basically allow
+  # the home page to not require sign in 
   skip_before_action :authenticate_user!, only: [:index]
   def index
   end
